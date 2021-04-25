93fd1e5786363e48e236e517c2ba1569d56c048a
Greg <gdrew21@gmail.com>
2021-04-25 15:21:48 -0400

    added express functionality

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ const path = require('path');
 
 console.log(path.join(__dirname, '../dist/index.html'))
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../dist/index.html')))
+app.use("/", express.static(path.join(__dirname, '/dist/')))
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/dist/')))
+
 app.listen(3000, () => console.log(`Example app listening at http://localhost:${3000}`))
